4ba718c1729880220463ac39042c94d9e50bea89
AugustoCalado <augusto.calado11@gmail.com>
2020-12-27 20:26:48 -0300

    C2 Node REST API - New Endpoints and RDS Integration

--- 2-Full-Stack-on-AWS/Project/udacity-c2-restapi/src/config/config.ts
+++ 2-Full-Stack-on-AWS/Project/udacity-c2-restapi/src/config/config.ts
@@ -1,11 +1,11 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "username": "udagramaugdev",
+    "password": "123",
+    "database": "udagramaugdev",
+    "host": "udagramaugdev.comj6e1l4su5.sa-east-1.rds.amazonaws.com",
+    "dialect": "postgress",
+    "aws_region": "sa-east-1c",
     "aws_profile": "default",
     "aws_media_bucket": "udagram-ruttner-dev"
   },
