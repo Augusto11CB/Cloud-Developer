f6c11ce2f0010fbd3852cb89b547f64552838ac8
AugustoCalado <augusto.calado11@gmail.com>
2020-12-29 09:51:55 -0300

    C2 Node REST API - Configuring project to use credentials in the environment variables

--- 2-Full-Stack-on-AWS/Project/udacity-c2-restapi/src/config/config.ts
+++ 2-Full-Stack-on-AWS/Project/udacity-c2-restapi/src/config/config.ts
@@ -1,12 +1,12 @@
 export const config = {
   "dev": {
-    "username": "udagramaugdev",
-    "password": "123",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
     "database": "udagramaugdev",
     "host": "udagramaugdev.comj6e1l4su5.sa-east-1.rds.amazonaws.com",
     "dialect": "postgress",
-    "aws_region": "sa-east-1",
-    "aws_profile": "default",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": "aug-udagram-dev"
   },
   "prod": {
