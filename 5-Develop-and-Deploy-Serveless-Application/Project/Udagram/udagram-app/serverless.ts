16e02e7a322abfd8e9fc1a3826d0726413b940a0
AugustoCalado <augusto.calado11@gmail.com>
2021-02-10 09:24:41 -0300

    C4 Added Lambda Function Created By Serverless Framework

---
+++ 5-Develop-and-Deploy-Serveless-Application/Project/Udagram/udagram-app/serverless.ts
@@ -0,0 +1,32 @@
+import type { AWS } from '@serverless/typescript';
+
+import { hello } from './src/functions';
+
+const serverlessConfiguration: AWS = {
+  service: 'udagram-app',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    region: 'sa-east-1',
+    stage: 'dev',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  functions: { hello }
+}
+
+module.exports = serverlessConfiguration;
