a4de0782d660e290f7a74feac2ff9883537c4fed
AugustoCalado <augusto.calado11@gmail.com>
2021-02-14 15:28:23 -0300

    C4 Udagram - Added Feature to generate presigned url

--- 5-Develop-and-Deploy-Serveless-Application/Project/Udagram/udagram-app-udacity-hands-on/src/lambda/http/createImage.ts
+++ 5-Develop-and-Deploy-Serveless-Application/Project/Udagram/udagram-app-udacity-hands-on/src/lambda/http/createImage.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
+import * as AWS from 'aws-sdk'
 import * as uuid from 'uuid'
 import * as middy from 'middy'
 // import { cors } from 'middy/middlewares'
@@ -8,14 +8,14 @@ import * as middy from 'middy'
 
 
 const docClient = new AWS.DynamoDB.DocumentClient()
-// const s3 = new XAWS.S3({
-//   signatureVersion: 'v4'
-// })
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
 
 const groupsTable = process.env.GROUPS_TABLE
 const imagesTable = process.env.IMAGES_TABLE
-// const bucketName = process.env.IMAGES_S3_BUCKET
-// const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+const bucketName = process.env.IMAGES_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Caller event', event)
@@ -34,13 +34,16 @@ export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGat
   const imageId = uuid.v4()
   const newItem = await createImage(groupId, imageId, event)
 
-//   const url = getUploadUrl(imageId)
+  const url = getUploadUrl(imageId)
 
   return {
     statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({
-      newItem: newItem
-    //   uploadUrl: url
+      newItem: newItem,
+      uploadUrl: url
     })
   }
 })
@@ -73,8 +76,8 @@ async function createImage(groupId: string, imageId: string, event: any) {
     groupId,
     timestamp,
     imageId,
-    ...newImage
-    // imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`
+    ...newImage,
+    imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`
   }
   console.log('Storing new item: ', newItem)
 
@@ -88,22 +91,27 @@ async function createImage(groupId: string, imageId: string, event: any) {
   return newItem
 }
 
-// function getUploadUrl(imageId: string) {
-//   return s3.getSignedUrl('putObject', {
-//     Bucket: bucketName,
-//     Key: imageId,
-//     Expires: urlExpiration
-//   })
-// }
+function getUploadUrl(imageId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: urlExpiration
+  })
+}
 
-// models:
-// - name: GroupRequest
-//   contentType: application/json
-//   schema: ${file(models/create-group-request.json)}
-// - name: ImageRequest
-//   contentType: application/json
-//   schema: ${file(models/create-image-request.json)}
+// custom:
+//   documentation:
+//     api:
+//       info:
+//         version: v1.0.0
+//         title: Udagram API
+//         description: Serverless application for images sharing
+//     models:
+//       - name: ImageRequest
+//         contentType: application/json
+//         schema: ${file(models/create-image-request.json)}
 
+// # Function
 // CreateImage:
 // handler: src/lambda/http/createImage.handler
 // events:
@@ -118,8 +126,55 @@ async function createImage(groupId: string, imageId: string, event: any) {
 //         requestModels:
 //           'application/json': ImageRequest
 
+// # Resources
+// AttachmentsBucket:
+// Type: AWS::S3::Bucket
+// DeletionPolicy: Retain
+// #DependsOn: SNSTopicPolicy
+// Properties:
+//   BucketName: ${self:provider.environment.IMAGES_S3_BUCKET}
+//   #NotificationConfiguration:
+//     #TopicConfigurations:
+//       #- Event: s3:ObjectCreated:Put
+//         #Topic: !Ref ImagesTopic
+//   CorsConfiguration:
+//     CorsRules:
+//       -
+//         AllowedOrigins:
+//           - '*'
+//         AllowedHeaders:
+//           - '*'
+//         AllowedMethods:
+//           - GET
+//           - PUT
+//           - POST
+//           - DELETE
+//           - HEAD
+//         MaxAge: 3000
+
+// BucketPolicy: # Allows anyone to get images from the bucket without be logged in
+// Type: AWS::S3::BucketPolicy
+// Properties:
+//   PolicyDocument:
+//     Id: MyPolicy
+//     Version: "2012-10-17"
+//     Statement:
+//       - Sid: PublicReadForGetBucketObjects
+//         Effect: Allow
+//         Principal: '*'
+//         Action: 's3:GetObject'
+//         Resource: 'arn:aws:s3:::${self:provider.environment.IMAGES_S3_BUCKET}/*'
+//   Bucket: !Ref AttachmentsBucket
+
+// # iamRoleStatements
 // - Effect: Allow
 // Action:
 //   - dynamodb:Query
 //   - dynamodb:PutItem
 // Resource: arn:aws:dynamodb:${self:provider.region}:*:table/${self:provider.environment.IMAGES_TABLE}
+
+// - Effect: Allow
+// Action: # generate a presigned url
+//   - s3:PutObject
+//   - s3:GetObject
+// Resource: arn:aws:s3:::${self:provider.environment.IMAGES_S3_BUCKET}/*
